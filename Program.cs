ae3f08356b8cc4aa40ba6c0b32626bf5b8a84223
kiragit01 <kiragithub@gmail.com>
2024-02-20 19:32:26 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 2 };
+            Console.WriteLine(MissingNumber(nums));
+            Console.ReadKey();
+        }
+
+        static int MissingNumber(int[] nums)
+        {
+            int n = nums.Length;
+            Array.Sort(nums);
+            for (int i = 0; i < n; i++)
+            {
+                if (i != nums[i]) return i;
+            }
+            return n;
+        }
+
+        static int MissingNumber_Bad(int[] nums)
+        {
+            int n = nums.Length;
+            int f = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == nums[j])
+                    {
+                        f = 1;
+                        break;
+                    }
+                    if (f == 0 && j == n - 1) return i;
+                }
+                f = 0;
+            }
+            return f;
+        }
+
+    }
+}
